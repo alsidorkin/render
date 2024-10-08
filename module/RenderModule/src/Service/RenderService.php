e09aeadcd46530189a6b3eb3a5c3be16df0cb6f8
alex <alexsidorkin567@gmail.com>
2024-10-08 22:57:10 +0300

    refactoring

--- module/RenderModule/src/Service/RenderService.php
+++ module/RenderModule/src/Service/RenderService.php
@@ -11,8 +11,6 @@ use Laminas\View\Model\ViewModel;
 
 /**
  * Class RenderService
- *
- * Сервис для рендеринга шаблонов.
  */
 class RenderService
 {
@@ -37,39 +35,78 @@ class RenderService
     public function renderAllTemplates(): array
     {
         $results = [];
+
+        $templateMap = $this->getTemplateMap();
+        if ($templateMap) {
+            foreach ($templateMap as $template => $templatePath) {
+                $results[$template] = $this->renderAndSaveTemplate($template);
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * @return array|null
+     */
+    private function getTemplateMap(): ?array
+    {
         /** @var ResolverInterface|null $resolver */
         $resolver = $this->renderer->resolver();
 
         if ($resolver instanceof \Laminas\View\Resolver\AggregateResolver) {
             foreach ($resolver->getIterator() as $templateResolver) {
                 if ($templateResolver instanceof \Laminas\View\Resolver\TemplateMapResolver) {
-                    // Получаем карту шаблонов из TemplateMapResolver
-                    $templateMap = $templateResolver->getMap();
-                    foreach ($templateMap as $template => $templatePath) {
-                        try {
-                            $viewModel = new ViewModel();
-                            $viewModel->setTemplate($template);
-                            $html = $this->renderer->render($viewModel);
-
-
-
-                            $filePath = __DIR__ . "/../../../test/Render/{$template}.html";
-                            $directoryPath = dirname($filePath);
-                            if (! is_dir($directoryPath)) {
-                                mkdir($directoryPath, 0777, true);
-                            }
-
-                            file_put_contents($filePath, $html);
-                            $results[$template] = $filePath;
-                        } catch (\Exception $e) {
-                            $results[$template] = "Error: " . $e->getMessage();
-                        }
-                    }
-                    break;
+                    return $templateResolver->getMap();
                 }
             }
         }
 
-        return $results;
+        return null;
+    }
+
+    /**
+     * @param string $template
+     * @return string
+     */
+    private function renderAndSaveTemplate(string $template): string
+    {
+        try {
+            $html = $this->renderTemplate($template);
+            $filePath = $this->saveTemplateToFile($template, $html);
+            return $filePath;
+        } catch (\Exception $e) {
+            return "Error: " . $e->getMessage();
+        }
+    }
+
+    /**
+     * @param string $template
+     * @return string
+     */
+    private function renderTemplate(string $template): string
+    {
+        $viewModel = new ViewModel();
+        $viewModel->setTemplate($template);
+        return $this->renderer->render($viewModel);
+    }
+
+    /**
+     * @param string $template
+     * @param string $html
+     * @return string
+     */
+    private function saveTemplateToFile(string $template, string $html): string
+    {
+        $filePath = __DIR__ . "/../../../test/Render/{$template}.html";
+        $directoryPath = dirname($filePath);
+
+        if (! is_dir($directoryPath)) {
+            mkdir($directoryPath, 0777, true);
+        }
+
+        file_put_contents($filePath, $html);
+
+        return $filePath;
     }
 }
